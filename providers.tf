3f34c2109a0d74d6031eb0b64718601dfcf95ce5
Charlie Dixon <charlie.dixon@lendscape.com>
2023-02-24 12:45:59 +0000

    rm token ref from k8s provider

--- providers.tf
+++ providers.tf
@@ -5,7 +5,6 @@ provider "aws" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.default.token
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
