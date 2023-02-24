b372fc5e90474218c9a58c866c53d8dc3edd6da8
Charlie Dixon <charlie.dixon@lendscape.com>
2023-02-24 15:13:53 +0000

    rm comment

--- variables.tf
+++ variables.tf
@@ -10,12 +10,6 @@ variable "linux_instance_type" {
   default     = "t3.small"
 }
 
-# variable "ubuntu_ami" {
-#   type        = string
-#   description = "Ubuntu 22.04 AMI"
-#   default     = "ami-0d09654d0a20d3ae2"
-# }
-
 variable "environment" {
   type        = string
   description = "Environment name"
