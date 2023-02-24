8a5bfa45aefe90a9cfedd787690c0ef9edb31e70
Charlie Dixon <charlie.dixon@lendscape.com>
2023-02-24 12:44:56 +0000

    rm unused data vars

--- data.tf
+++ data.tf
@@ -1,11 +1,3 @@
 data "aws_availability_zones" "available" {}
 
 data "aws_caller_identity" "current" {}
-
-data "aws_eks_cluster" "default" {
-  name = local.cluster_name
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = local.cluster_name
-}
