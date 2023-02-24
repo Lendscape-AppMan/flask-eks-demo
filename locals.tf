918e658dd8022cd5d0676d726e51cfa486fba426
Charlie Dixon <charlie.dixon@lendscape.com>
2023-02-24 12:37:20 +0000

    change cluster name

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
-  cluster_name = "containerized-deployment"
-  vpc_name     = "vpc-containerized-deployment-${var.environment}"
+  cluster_name = "flask-k8s-demo-deployment"
+  vpc_name     = "vpc-flask-k8s-demo-deployment-${var.environment}"
   region       = "eu-west-2"
   vpc_cidr     = "10.0.0.0/16"
   azs          = slice(data.aws_availability_zones.available.names, 0, 3)
--- providers.tf
+++ providers.tf
@@ -3,8 +3,9 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
@@ -21,7 +22,7 @@ provider "helm" {
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+      args        = ["eks", "get-token", "--cluster-name", local.cluster_name]
     }
   }
 }
