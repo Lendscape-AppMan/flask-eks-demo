4ba1697aa1dcc46d701543f4a4332c1edaf8ada6
Charlie Dixon <charlie.dixon@lendscape.com>
2023-02-27 11:03:12 +0000

    modify argocd helm release to remove argocd-apps usage

--- argocd.tf
+++ argocd.tf
@@ -5,14 +5,6 @@ resource "helm_release" "argocd" {
   version          = "5.17.1"
   repository       = "https://argoproj.github.io/argo-helm"
   create_namespace = true
-}
-
-resource "helm_release" "argocd-apps" {
-  depends_on = [helm_release.argocd]
-  chart      = "argocd-apps"
-  name       = "argocd-apps"
-  namespace  = "argocd"
-  repository = "https://argoproj.github.io/argo-helm"
   values = [
     file("argocd/application.yaml")
   ]
