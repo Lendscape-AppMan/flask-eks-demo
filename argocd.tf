9dff26cd363243e1dd5b38566289c29c13c12b7c
Charlie Dixon <charlie.dixon@lendscape.com>
2023-02-27 10:46:08 +0000

    use dependent argocd-apps helm release resource

--- argocd.tf
+++ argocd.tf
@@ -1,14 +1,19 @@
 resource "helm_release" "argocd" {
-  name  = "argocd"
-
-  repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
+  name             = "argocd"
   namespace        = "argocd"
   version          = "5.17.1"
+  repository       = "https://argoproj.github.io/argo-helm"
   create_namespace = true
-  wait             = true
-  lint             = true
+}
 
+resource "helm_release" "argocd-apps" {
+  depends_on = [helm_release.argocd]
+  chart      = "argocd-apps"
+  name       = "argocd-apps"
+  namespace  = "argocd"
+  version    = "5.17.1"
+  repository = "https://argoproj.github.io/argo-helm"
   values = [
     file("argocd/flask-application.yaml")
   ]
